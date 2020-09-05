3318ee9958f7d16931bf134c616b12590ff86cae
Patrick Purcell <patrick.purcell@intel.com>
2020-09-05 12:44:34 -0700

    Updating vk code gen

--- dynamic_static.cmake
+++ dynamic_static.cmake
@@ -6,11 +6,25 @@
 #     http://opensource.org/licenses/MIT
 # ==========================================
 
+message("Pre include_guard() : ${CMAKE_CURRENT_LIST_DIR}")
+
 include_guard()
+
+message("Post include_guard() : ${CMAKE_CURRENT_LIST_DIR}")
 
 include(CheckCxxCompilerFlag)
 include(CmakeParseArguments)
 include(ExternalProject)
+
+# TODO : Documentation
+function(dst_add_subdirectory dstDependency)
+    set(dstDependencySourceDirectory "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../${dstDependency}/")
+    get_filename_component(dstDependencySourceDirectory "${dstDependencySourceDirectory}" REALPATH)
+    get_directory_property(subdirectories DIRECTORY "${CMAKE_SOURCE_DIR}" SUBDIRECTORIES)
+    if(NOT "${dstDependencySourceDirectory}" IN_LIST subdirectories)
+        add_subdirectory("${dstDependencySourceDirectory}" "${CMAKE_BINARY_DIR}/dynamic_static/${dstDependency}")
+    endif()
+endfunction()
 
 # TODO : Documentation
 function(dst_create_file_group files)
@@ -29,9 +43,9 @@ endfunction()
 # TODO : Documentation
 function(dst_setup_target)
     cmake_parse_arguments(args "" "target;folder" "includeDirectories;includeFiles;sourceFiles;linkLibraries;compileDefinitions" ${ARGN})
-    target_include_directories(${args_target} "${args_includeDirectories}")
+    target_include_directories(${args_target} PUBLIC "${args_includeDirectories}")
     target_link_libraries(${args_target} "${args_linkLibraries}")
-    target_compile_definitions(${args_target} "${args_compileDefinitions}")
+    target_compile_definitions(${args_target} PUBLIC "${args_compileDefinitions}")
     set_target_properties(${args_target} PROPERTIES LINKER_LANGUAGE CXX)
     target_compile_features(${args_target} PUBLIC cxx_std_17)
     dst_create_file_group("${args_includeFiles}")
@@ -69,4 +83,27 @@ function(dst_add_static_library)
     cmake_parse_arguments(args "" "target" "includeFiles;sourceFiles;compileDefinitions" ${ARGN})
     add_library(${args_target} STATIC "${args_includeFiles}" "${args_sourceFiles}")
     dst_setup_target(${ARGN})
+endfunction()
+
+# TODO : Documentation
+function(dst_add_test_suite)
+    cmake_parse_arguments(args "" "target" "includeDirectories;includeFiles;sourceFiles;compileDefinitions" ${ARGN})
+    set(catchSourceDirectory "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/external/Catch2/")
+    set(catchBinaryDirectory "${CMAKE_CURRENT_BINARY_DIR}/catch2/")
+    set(catchCpp "${catchBinaryDirectory}/catch.cpp")
+    if(NOT TARGET Catch2::Catch2)
+        add_subdirectory("${catchSourceDirectory}" "${catchBinaryDirectory}")
+    endif()
+    if(NOT EXISTS ${catchCpp})
+        file(WRITE "${catchCpp}" "\n#define CATCH_CONFIG_MAIN\n#include \"catch2/catch.hpp\"\n")
+    endif()
+    dst_add_executable(
+        target ${args_target}.tests
+        folder ${args_target}
+        linkLibraries ${args_target} Catch2::Catch2
+        includeDirectories "${args_includeDirectories}"
+        sourceFiles "${args_sourceFiles}" "${catchCpp}"
+    )
+    enable_testing()
+    add_test(NAME ${args_target}.tests COMMAND ${args_target}.tests)
 endfunction()
